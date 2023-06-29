a4fd4104335ab7db5ee18a019bbe7822577c5700
Raphael Brasil <raphael.brasil>
2023-06-29 16:13:01 -0300

    feat: creat new cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,39 @@
-import { CardLayout } from './styled'
+import { CardLayout, CardTop, CardBottom, CardBottomTags, CardBottomBtn, CardContent, CardAuthor, CardSynthesis, CardTopBoxBtn  } from './styled'
+import { Button } from '../../components'
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai"
 
-export const Card = () => <CardLayout>CARD</CardLayout>
+export const Card = () =>
+  <CardLayout>
+    <CardTop>
+      <CardContent>
+        <CardAuthor>
+          <strong>Autor:</strong> João Soares
+        </CardAuthor>
+        <CardSynthesis>
+        "O texto descritivo é um tipo textual marcado pela descrição de um lugar ou paisagem, animal, pessoa, acontecimento, entre outros, tendo como objetivo trazer uma imagem ou cena ao seu leitor. Geralmente é utilizado junto ao texto narrativo, pois as sequências formadas pela narração são intercala..."
+        </CardSynthesis>
+      </CardContent>
+      <CardTopBoxBtn>
+        <Button icon outline onClick={() => {}}>
+          <AiOutlineEdit />
+        </Button>
+        <Button icon outline color='DANGER' onClick={() => {}}>
+          <AiOutlineDelete/>
+        </Button>
+      </CardTopBoxBtn>
+      
+    </CardTop>
+    <CardBottom>
+      <CardBottomTags>
+        <div>Aplicação</div>
+        <div>Time</div>
+        <div>Plataforma</div>
+        <div>Status</div>
+      </CardBottomTags>
+      <CardBottomBtn>
+        <Button onClick={() => {}}>
+          Ver mais
+        </Button>
+      </CardBottomBtn>
+    </CardBottom>
+  </CardLayout>
--- src/components/Card/styled.ts
+++ src/components/Card/styled.ts
@@ -2,5 +2,59 @@ import styled from 'styled-components'
 
 export const CardLayout = styled.div`
   width: 100%;
-  background-color: red;
+  max-width: calc(100% - ${({ theme }) => theme.spaceSize.large}px);
+  height: 162px;
+  max-height: 162px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.borderMain};
+  border-radius: 6px;
+  padding: ${({ theme }) => theme.spaceSize.base}px;
+`
+
+export const CardTop = styled.div`
+  display: flex;
+  width: 100%;
+  height: calc(100% - 32px);
+`
+
+export const CardBottom = styled.div`
+  display: flex;
+  width: 100%;
+  height: 32px;
+`
+
+export const CardBottomTags = styled.div`
+  display: flex;
+  width: calc(100% - 110px);
+`
+
+export const CardBottomBtn = styled.div`
+  width: 40px;
+`
+
+export const CardContent = styled.div`  
+  max-height: 100%;
+`
+
+export const CardAuthor = styled.div`
+  width: calc(100% - ${({ theme }) => theme.spaceSize.xsmall}px);
+  height: 28px;
+`
+
+export const CardSynthesis = styled.div`
+  display: -webkit-box;
+  width: calc(100% - ${({ theme }) => theme.spaceSize.xsmall}px);
+  max-height: calc(100% - 36px);
+  white-space: normal;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+`
+
+export const CardTopBoxBtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: ${({ theme }) => theme.spaceSize.xsmall}px;
 `
